7dd6546b1036532f150c8c14ef94c4f94acb04ae
Veeranagouda Ganji <shivakumarganjiatme@gmail.com>
2023-10-14 11:58:25 +0530

    assignment 9

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -38,6 +38,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart=(req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.findById(prodId,products=>{
+    Cart.addProduct(prodId,products.price)
+  })
+  res.redirect('/cart')
+}
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
